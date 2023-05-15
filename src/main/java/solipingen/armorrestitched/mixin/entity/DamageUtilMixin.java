2edb9e068d7720452ac7170400ce4e0cccb8002a
SolipIngen <eventhorizon108@gmail.com>
2023-05-14 18:00:53 -0700

    Armour damage intake readjustments

--- src/main/java/solipingen/armorrestitched/mixin/entity/DamageUtilMixin.java
+++ src/main/java/solipingen/armorrestitched/mixin/entity/DamageUtilMixin.java
@@ -17,14 +17,14 @@ public abstract class DamageUtilMixin {
     @Inject(method = "getDamageLeft", at = @At("HEAD"), cancellable = true)
     private static void injectedGetDamageLeft(float damage, float armor, float armorToughness, CallbackInfoReturnable<Float> cbireturn) {
         float f = 1.0f + armorToughness/4.0f;
-        float g = MathHelper.clamp(2.0f*armor - damage/f, armor*0.1f, 50.0f);
+        float g = MathHelper.clamp(2.0f*armor - damage/f, 0.1f*armor, 50.0f);
         cbireturn.setReturnValue(damage*(1.0f - g/50.0f));
     }
 
     @Inject(method = "getInflictedDamage", at = @At("HEAD"), cancellable = true)
     private static void injectedGetInflictedDamage(float damageDealt, float protection, CallbackInfoReturnable<Float> cbireturn) {
-        float f = MathHelper.clamp(2.25f*protection, 0.0f, 48.0f);
-        cbireturn.setReturnValue(damageDealt*(1.0f - f/50.0f));
+        float f = MathHelper.clamp(1.75f*protection, 0.0f, 37.0f);
+        cbireturn.setReturnValue(damageDealt*(1.0f - f/40.0f));
     }
 
 
