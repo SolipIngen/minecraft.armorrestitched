5a2b925baa9ca711a2e56eb5bf834e011899e3c6
SolipIngen <eventhorizon108@gmail.com>
2024-06-05 23:01:42 -0700

    Mod armour durability fixed.

--- src/main/java/solipingen/armorrestitched/item/ModItems.java
+++ src/main/java/solipingen/armorrestitched/item/ModItems.java
@@ -17,99 +17,99 @@ public class ModItems {
 
     // Cotton Clothing
     public static final Item COTTON_HELMET = ModItems.registerItem("cotton_helmet",
-            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(9))));
 
     public static final Item COTTON_CHESTPLATE = ModItems.registerItem("cotton_chestplate",
-            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(9))));
 
     public static final Item COTTON_LEGGINGS = ModItems.registerItem("cotton_leggings",
-            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(9))));
 
     public static final Item COTTON_BOOTS = ModItems.registerItem("cotton_boots",
-            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.COTTON, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(9))));
 
 
     // Fur Clothing
     public static final Item FUR_HELMET = ModItems.registerItem("fur_helmet",
-            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(9))));
 
     public static final Item FUR_CHESTPLATE = ModItems.registerItem("fur_chestplate",
-            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(9))));
 
     public static final Item FUR_LEGGINGS = ModItems.registerItem("fur_leggings",
-            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(9))));
 
     public static final Item FUR_BOOTS = ModItems.registerItem("fur_boots",
-            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.FUR, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(9))));
 
 
     // Linen Clothing
     public static final Item LINEN_HELMET = ModItems.registerItem("linen_helmet",
-            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(9))));
 
     public static final Item LINEN_CHESTPLATE = ModItems.registerItem("linen_chestplate",
-            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(9))));
 
     public static final Item LINEN_LEGGINGS = ModItems.registerItem("linen_leggings",
-            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(9))));
 
     public static final Item LINEN_BOOTS = ModItems.registerItem("linen_boots",
-            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.LINEN, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(9))));
 
 
     // Silk Clothing
     public static final Item SILK_HELMET = ModItems.registerItem("silk_helmet",
-            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(9))));
 
     public static final Item SILK_CHESTPLATE = ModItems.registerItem("silk_chestplate",
-            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(9))));
 
     public static final Item SILK_LEGGINGS = ModItems.registerItem("silk_leggings",
-            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(9))));
 
     public static final Item SILK_BOOTS = ModItems.registerItem("silk_boots",
-            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.SILK, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(9))));
 
 
     // Wool Clothing
     public static final Item WOOL_HELMET = ModItems.registerItem("wool_helmet",
-            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(9))));
 
     public static final Item WOOL_CHESTPLATE = ModItems.registerItem("wool_chestplate",
-            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(9))));
 
     public static final Item WOOL_LEGGINGS = ModItems.registerItem("wool_leggings",
-            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(9))));
 
     public static final Item WOOL_BOOTS = ModItems.registerItem("wool_boots",
-            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(9)))));
+            new ArmorItem(ModArmorMaterials.WOOL, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(9))));
 
     // Paper Clothing
     public static final Item PAPER_HELMET = ModItems.registerItem("paper_helmet",
-            new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(5)))));
+            new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(5))));
 
     public static final Item PAPER_CHESTPLATE = ModItems.registerItem("paper_chestplate",
-            new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(5)))));
+            new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(5))));
 
     public static final Item PAPER_LEGGINGS = ModItems.registerItem("paper_leggings", 
-        new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(5)))));
+        new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(5))));
 
     public static final Item PAPER_BOOTS = ModItems.registerItem("paper_boots",
-            new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(5)))));
+            new ArmorItem(ModArmorMaterials.PAPER, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(5))));
 
 
     // Copper Armor
     public static final Item COPPER_HELMET = ModItems.registerItem("copper_helmet", 
-        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.HELMET.getMaxDamage(15)))));
+        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.HELMET, new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(15))));
 
     public static final Item COPPER_CHESTPLATE = ModItems.registerItem("copper_chestplate", 
-        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.CHESTPLATE.getMaxDamage(15)))));
+        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.CHESTPLATE, new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(15))));
 
     public static final Item COPPER_LEGGINGS = ModItems.registerItem("copper_leggings", 
-        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.LEGGINGS.getMaxDamage(15)))));
+        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.LEGGINGS, new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(15))));
 
     public static final Item COPPER_BOOTS = ModItems.registerItem("copper_boots", 
-        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(MathHelper.ceil(1.75f*ArmorItem.Type.BOOTS.getMaxDamage(15)))));
+        new ArmorItem(ModArmorMaterials.COPPER, ArmorItem.Type.BOOTS, new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(15))));
 
     public static final Item COPPER_HORSE_ARMOR = ModItems.registerItem("copper_horse_armor", 
         new AnimalArmorItem(ModArmorMaterials.COPPER, AnimalArmorItem.Type.EQUESTRIAN, false, new Item.Settings().maxCount(1)));
